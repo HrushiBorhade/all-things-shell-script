a28873fc185ed7f7af94505f400ee0ae11ac38d9
HrushiBorhade <hrushiborhade123@gmail.com>
2025-03-15 13:52:28 +0530

    feat: Add arguments script

---
+++ arguments.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "File name: $0"
+echo "First argument: $1"
+echo "Second argument: $2"
+
+<< comment
+example
+➜  all-things-shell-script git:(main) ✗ bash arguments.sh hrushi borhade
+File name: arguments.sh
+First argument: hrushi
+Second argument: borhade
+comment
